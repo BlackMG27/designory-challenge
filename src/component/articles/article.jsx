e4e30616e7eb4b21f8aaa9528d1c6835c532bca5
BlackMG27 <oike279@gmail.com>
2021-03-14 18:16:28 -0400

    got the articles barebones structure

---
+++ src/component/articles/article.jsx
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+
+const Article = () => (
+    <Fragment>
+        <figure className="article-item">
+            <div className="article-item__img"></div>
+            <h2 className="article-item__title"></h2>
+            <h3 className="article-item__subtitle"></h3>
+            <p className="article-item__text"></p>
+        </figure>
+    </Fragment>
+)
+
+export default Article;
---
+++ src/component/articles/articles.jsx
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react';
+import Article from './article';
+
+const Articles = () => (
+    <Fragment>
+
+    </Fragment>
+);
+
+export default Articles;
