9a348863f71c7ed31263c921df0e295378d7c796
BlackMG27 <oike279@gmail.com>
2021-03-15 14:32:12 -0400

    added the notes and the desktop styling

--- src/component/hero/hero.jsx
+++ src/component/hero/hero.jsx
@@ -5,12 +5,14 @@ import Button from './../button/button';
 const Hero = () => (
     <Fragment>
         <section className="hero-section">
-            <h2 className="subtitle">In Honor of Women's History Month</h2>
-            <h1 className="title"> Badass Women in History</h1>
-            <Button 
-            text="Check Them Out!"
-            label="Learn More about Badass Women in History"
-            />
+            <div className="hero-section__content">
+                <h2 className="hero-section__subtitle">In Honor of Women's History Month</h2>
+                <h1 className="hero-section__title"> Badass Women in History</h1>
+                <Button 
+                    text="Check Them Out!"
+                    label="Learn More about Badass Women in History"
+                />
+            </div>
         </section>
     </Fragment>
 )
